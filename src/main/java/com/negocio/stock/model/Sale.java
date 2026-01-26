55508dfd0ec541d778b4ad72160473d1f4407f00
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-25 22:51:03 -0300

    se definieron los atributos, los constructores y como se va a comportar la clase como entidad en la base de datos
    Sale.java

--- src/main/java/com/negocio/stock/model/Sale.java
+++ src/main/java/com/negocio/stock/model/Sale.java
@@ -1,4 +1,30 @@
 package com.negocio.stock.model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "sales")
 public class Sale {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime date;
+    private BigDecimal total;
+
+    private Seller seller;
+    private List<SaleDetail> ticket;
+
+    public Sale() {
+    }
+
+    public Sale(LocalDateTime date, BigDecimal total, Seller seller, List<SaleDetail> ticket) {
+        this.date = date;
+        this.total = total;
+        this.seller = seller;
+        this.ticket = ticket;
+    }
 }
