c3eafc191d3049358f7b71a9d71af76002898abe
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-26 02:21:00 -0300

    se implementaron las relaciones y como se deben manejar en la base de datos
    SaleDetail.java

--- src/main/java/com/negocio/stock/model/SaleDetail.java
+++ src/main/java/com/negocio/stock/model/SaleDetail.java
@@ -10,10 +10,18 @@ public class SaleDetail {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
     private Product product;
+
     private int quantity;
     private BigDecimal unitPrice;
     private BigDecimal itemTotal;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sale_id", nullable = false)
+    private Sale sale;
 
     public SaleDetail() {
     }
