5414c2042ed772e98a1867cfb836763d9263f7c6
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-26 00:06:30 -0300

    se definieron los atributos, los constructores y como se va a comportar la clase como entidad en la base de datos
    Seller.java

--- src/main/java/com/negocio/stock/model/Seller.java
+++ src/main/java/com/negocio/stock/model/Seller.java
@@ -1,4 +1,20 @@
 package com.negocio.stock.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "sellers")
 public class Seller {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    // UserSec usersec;
+
+    public Seller() {
+    }
+
+    public Seller(String name) {
+        this.name = name;
+    }
 }
