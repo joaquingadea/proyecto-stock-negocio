e47c7a56d78f2227c71d0a73e63969ee0383de96
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-25 22:37:07 -0300

    se definieron los atributos, los constructores y como se va a comportar la clase como entidad en la base de datos
    Product.java

--- src/main/java/com/negocio/stock/model/Product.java
+++ src/main/java/com/negocio/stock/model/Product.java
@@ -1,4 +1,26 @@
 package com.negocio.stock.model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String name;
+    private BigDecimal price;
+    private String description;
+
+    public Product() {
+    }
+
+    public Product(String name, BigDecimal price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
 }
