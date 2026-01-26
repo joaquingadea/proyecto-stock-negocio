471ca3bd0bb9305badad5acc466c4986bd67bae3
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-25 22:23:32 -0300

    se definieron las annotations para el controller
    PruebasController.java

--- src/main/java/com/negocio/stock/controller/PruebasController.java
+++ src/main/java/com/negocio/stock/controller/PruebasController.java
@@ -1,4 +1,9 @@
 package com.negocio.stock.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pruebas")
 public class PruebasController {
 }
