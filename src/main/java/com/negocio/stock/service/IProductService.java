70fdc7ba07d90cb8f7e1af3de3fa40af4df73292
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-26 13:54:58 -0300

    se modifico el metodo create para que devuelva un Product.java y reciba como parametro un CreateProductDTO.java
    IProductService.java

--- src/main/java/com/negocio/stock/service/IProductService.java
+++ src/main/java/com/negocio/stock/service/IProductService.java
@@ -1,8 +1,9 @@
 package com.negocio.stock.service;
 
+import com.negocio.stock.dto.CreateProductDTO;
 import com.negocio.stock.model.Product;
 
 public interface IProductService {
-    void create(Product product);
+    Product create(CreateProductDTO request);
     Product read(Long id);
 }
