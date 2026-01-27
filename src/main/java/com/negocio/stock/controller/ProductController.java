d490cbd46a6f4a4f25a218095a6dae17218dd4fc
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-27 01:53:35 -0300

    se incorporo el endpoint para buscar un producto por id
    ProductController.java

--- src/main/java/com/negocio/stock/controller/ProductController.java
+++ src/main/java/com/negocio/stock/controller/ProductController.java
@@ -1,15 +1,13 @@
 package com.negocio.stock.controller;
 
 import com.negocio.stock.dto.CreateProductDTO;
+import com.negocio.stock.model.Product;
 import com.negocio.stock.repository.IProductRepository;
 import com.negocio.stock.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
@@ -24,4 +22,9 @@ public class ProductController {
                 .body(productService.create(request));
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> read(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(productService.read(id));
+    }
 }
