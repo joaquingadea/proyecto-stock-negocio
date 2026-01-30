f95e59e057fbeb7f456fba1b1eefe48894a0101f
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-29 22:40:19 -0300

    Se declararon sus atributos, constructores y como se va a comportar la entidad en la base de datos
    Permission.java

--- src/main/java/com/negocio/stock/model/Permission.java
+++ src/main/java/com/negocio/stock/model/Permission.java
@@ -1,4 +1,20 @@
 package com.negocio.stock.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name= "permissions")
 public class Permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String name;
+
+    public Permission() {
+    }
+
+    public Permission(String name) {
+        this.name = name;
+    }
 }
