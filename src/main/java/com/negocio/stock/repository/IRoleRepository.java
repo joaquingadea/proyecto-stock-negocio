1a91686462623e3f5f5619f7d27718bf0e1ddb68
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-30 17:34:35 -0300

    Se agregó la annotation Repository correspondiente.
    IPermissionRepository.java, IRoleRepository.java y IUserSecRepository.java

--- src/main/java/com/negocio/stock/repository/IRoleRepository.java
+++ src/main/java/com/negocio/stock/repository/IRoleRepository.java
@@ -2,6 +2,8 @@ package com.negocio.stock.repository;
 
 import com.negocio.stock.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IRoleRepository extends JpaRepository<Role,Long> {
 }
--- src/main/java/com/negocio/stock/repository/IUserSecRepository.java
+++ src/main/java/com/negocio/stock/repository/IUserSecRepository.java
@@ -2,9 +2,11 @@ package com.negocio.stock.repository;
 
 import com.negocio.stock.model.UserSec;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface IUserSecRepository extends JpaRepository<UserSec,Long> {
     Optional<UserSec> findUserByUsername(String username);
 }
