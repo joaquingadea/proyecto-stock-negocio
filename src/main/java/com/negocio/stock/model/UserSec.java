9473c0276e694a108a3881b4727551aecd713f04
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-29 22:14:23 -0300

    Se declararon sus atributos, constructores y como se va a comportar la entidad en la base de datos (relacion n a n unidireccional de UserSec a Role)
    UserSec.java

--- src/main/java/com/negocio/stock/model/UserSec.java
+++ src/main/java/com/negocio/stock/model/UserSec.java
@@ -1,4 +1,43 @@
 package com.negocio.stock.model;
 
+import jakarta.persistence.*;
+import org.jspecify.annotations.NonNull;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
 public class UserSec {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roleList = new HashSet<>();
+
+    public UserSec() {
+    }
+
+    public UserSec(String username, String password, boolean accountNonExpired, boolean accountNonLocked, boolean credentialsNonExpired, boolean enabled, Set<Role> roleList) {
+        this.username = username;
+        this.password = password;
+        this.accountNonExpired = accountNonExpired;
+        this.accountNonLocked = accountNonLocked;
+        this.credentialsNonExpired = credentialsNonExpired;
+        this.enabled = enabled;
+        this.roleList = roleList;
+    }
 }
