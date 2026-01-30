b0f0e0f996c524b91bbd6242b91f96d66fd2f971
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-30 17:29:04 -0300

    Se agregó el metodo findUserByUsername
    IUserSecRepository.java

--- src/main/java/com/negocio/stock/repository/IUserSecRepository.java
+++ src/main/java/com/negocio/stock/repository/IUserSecRepository.java
@@ -3,5 +3,8 @@ package com.negocio.stock.repository;
 import com.negocio.stock.model.UserSec;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IUserSecRepository extends JpaRepository<UserSec,Long> {
+    Optional<UserSec> findUserByUsername(String username);
 }
