31a6a8bfaebc41dc9e0eb19aaf02f830c42bd453
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-30 17:36:01 -0300

    Se implementó el método loadUserByUsername
    UserDetailsServiceImp.java

--- src/main/java/com/negocio/stock/service/UserDetailsServiceImp.java
+++ src/main/java/com/negocio/stock/service/UserDetailsServiceImp.java
@@ -1,4 +1,46 @@
 package com.negocio.stock.service;
 
-public class UserDetailsServiceImp {
+import com.negocio.stock.model.UserSec;
+import com.negocio.stock.repository.IUserSecRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImp implements UserDetailsService {
+
+    @Autowired
+    private IUserSecRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserSec userSec = userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid username or password."));
+
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        userSec.getRoleList().stream()
+                .flatMap(role -> role.getPermissionList().stream())
+                .forEach(permission -> authorities.add(new SimpleGrantedAuthority(permission.getName())));
+
+        userSec.getRoleList().stream()
+                .forEach(role -> authorities.add(new SimpleGrantedAuthority("ROLE_".concat(role.getName()))));
+
+        return new User(
+                userSec.getUsername(),
+                userSec.getUsername(),
+                userSec.isEnabled(),
+                userSec.isAccountNonExpired(),
+                userSec.isCredentialsNonExpired(),
+                userSec.isAccountNonLocked(),
+                authorities
+        );
+    }
 }
