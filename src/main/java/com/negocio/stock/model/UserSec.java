39ec411fc22e74c22dfa6e3f02acf233c090dad0
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-29 22:31:26 -0300

    Se eliminó el fetch de la relacion N a N
    UserSec.java

--- src/main/java/com/negocio/stock/model/UserSec.java
+++ src/main/java/com/negocio/stock/model/UserSec.java
@@ -22,7 +22,7 @@ public class UserSec {
     private boolean credentialsNonExpired;
     private boolean enabled;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
