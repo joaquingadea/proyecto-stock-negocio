5214c99254de14dd1f9db4d788f413a7689df568
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-30 18:12:13 -0300

    Se agregaron las implementaciones de las interfaces correspondientes a las clases PermissionService, RoleService y UserSecService

--- src/main/java/com/negocio/stock/service/RoleService.java
+++ src/main/java/com/negocio/stock/service/RoleService.java
@@ -3,5 +3,5 @@ package com.negocio.stock.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RoleService {
+public class RoleService implements IRoleService{
 }
--- src/main/java/com/negocio/stock/service/UserSecService.java
+++ src/main/java/com/negocio/stock/service/UserSecService.java
@@ -3,5 +3,5 @@ package com.negocio.stock.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserSecService {
+public class UserSecService implements IUserSecService{
 }
