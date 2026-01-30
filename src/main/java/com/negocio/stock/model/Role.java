fab290bbb46fbbb59b36c3c99f274eb19eaa9f01
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-01-29 22:32:15 -0300

    Se declararon sus atributos, constructores y como se va a comportar la entidad en la base de datos (relacion n a n unidireccional de Role a Permission)
    Role.java

--- src/main/java/com/negocio/stock/model/Role.java
+++ src/main/java/com/negocio/stock/model/Role.java
@@ -1,4 +1,29 @@
 package com.negocio.stock.model;
 
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String name;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "role_permission",
+    joinColumns = @JoinColumn(name = "role_id"),
+    inverseJoinColumns = @JoinColumn(name = "permission_id"))
+    private Set<Permission> permissionList = new HashSet<>();
+
+    public Role() {
+    }
+
+    public Role(String name, Set<Permission> permissionList) {
+        this.name = name;
+        this.permissionList = permissionList;
+    }
 }
