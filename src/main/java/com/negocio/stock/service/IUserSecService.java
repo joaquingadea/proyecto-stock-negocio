c07ea3b4c11f8ad3cca785febb34eb04244900bb
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-02 20:10:54 -0300

    Creación del metodo register
    IUserService.java

--- src/main/java/com/negocio/stock/service/IUserSecService.java
+++ src/main/java/com/negocio/stock/service/IUserSecService.java
@@ -1,4 +1,7 @@
 package com.negocio.stock.service;
 
+import com.negocio.stock.dto.AuthRegisterRequestDTO;
+import com.negocio.stock.dto.AuthRegisterResponseDTO;
 public interface IUserSecService {
+    AuthRegisterResponseDTO register(AuthRegisterRequestDTO request);
 }
