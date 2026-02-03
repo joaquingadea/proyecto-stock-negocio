4d5405a2bbc4273cf42e6aa7d16452c3c1f12f05
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-02 21:31:41 -0300

    Se agrego el metodo create.

--- src/main/java/com/negocio/stock/service/IPermissionService.java
+++ src/main/java/com/negocio/stock/service/IPermissionService.java
@@ -1,4 +1,8 @@
 package com.negocio.stock.service;
 
+import com.negocio.stock.model.Permission;
+import org.jspecify.annotations.Nullable;
+
 public interface IPermissionService {
+    Permission create(Permission permission);
 }
