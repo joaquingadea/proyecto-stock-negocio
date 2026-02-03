8e40a08ac368b4ec5f7cd84ce63bb3bdd9057f96
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-03 15:43:20 -0300

    Se implementó el registro necesario para el manejo de las peticiones del endpoint login implementando Bean Validation.
    AuthLoginRequestDTO.java

--- src/main/java/com/negocio/stock/dto/AuthLoginRequestDTO.java
+++ src/main/java/com/negocio/stock/dto/AuthLoginRequestDTO.java
@@ -1,4 +1,12 @@
 package com.negocio.stock.dto;
 
-public record AuthLoginRequestDTO() {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record AuthLoginRequestDTO(
+        @NotBlank
+        String username,
+        @NotBlank
+        @Size(min = 8)
+        String password
+){}
