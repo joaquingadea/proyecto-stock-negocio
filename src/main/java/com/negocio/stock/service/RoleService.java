a5cbb27cd992a6f3064489b1babead0b6c67eb0e
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-02 21:33:56 -0300

    Se implementó el metodo create y findById.
    RoleService.java

--- src/main/java/com/negocio/stock/service/RoleService.java
+++ src/main/java/com/negocio/stock/service/RoleService.java
@@ -1,7 +1,25 @@
 package com.negocio.stock.service;
 
+import com.negocio.stock.model.Role;
+import com.negocio.stock.repository.IRoleRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoleService implements IRoleService{
+
+    @Autowired
+    private IRoleRepository roleRepository;
+
+    @Override
+    public Role create(Role role) {
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public Role findById(Long id) {
+        return roleRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("No se encontró el rol"));
+    }
 }
