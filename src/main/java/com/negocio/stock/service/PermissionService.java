a01aaada3ab6bd0f81fba3212f3d49ed15f59dc7
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-02 21:33:12 -0300

    Se implementó el metodo create.
    PermissionService.java

--- src/main/java/com/negocio/stock/service/PermissionService.java
+++ src/main/java/com/negocio/stock/service/PermissionService.java
@@ -1,7 +1,18 @@
 package com.negocio.stock.service;
 
+import com.negocio.stock.model.Permission;
+import com.negocio.stock.repository.IPermissionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PermissionService implements IPermissionService{
+
+    @Autowired
+    private IPermissionRepository permissionRepository;
+
+    @Override
+    public Permission create(Permission permission) {
+        return permissionRepository.save(permission);
+    }
 }
