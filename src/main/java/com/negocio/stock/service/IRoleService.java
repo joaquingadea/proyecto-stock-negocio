2e46919280f55b5072fee6359d1b8f861ece8b86
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-02 21:32:14 -0300

    Se agrego el metodo create y findById.
    IRoleService.java

--- src/main/java/com/negocio/stock/service/IRoleService.java
+++ src/main/java/com/negocio/stock/service/IRoleService.java
@@ -1,4 +1,9 @@
 package com.negocio.stock.service;
 
+import com.negocio.stock.model.Role;
+import org.jspecify.annotations.Nullable;
+
 public interface IRoleService {
+    Role create(Role role);
+    Role findById(Long id);
 }
