33fc69348b22e226b3e025670f7c2f375fc351da
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-02 21:26:29 -0300

    Se creó e implementó el endpoint create
    PermissionController.java

--- src/main/java/com/negocio/stock/controller/PermissionController.java
+++ src/main/java/com/negocio/stock/controller/PermissionController.java
@@ -1,4 +1,25 @@
 package com.negocio.stock.controller;
 
+import com.negocio.stock.model.Permission;
+import com.negocio.stock.service.IPermissionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/permission")
 public class PermissionController {
+
+    @Autowired
+    private IPermissionService permissionService;
+
+    @PostMapping
+    public ResponseEntity<Permission> create(@RequestBody Permission permission){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(permissionService.create(permission));
+    }
 }
