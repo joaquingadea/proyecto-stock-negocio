2c28dd43765e9f87e4b1865be49e92c643d88ef9
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-02 21:25:27 -0300

    Se crearon e implementaron los metodos create y getById
    RoleController.java

--- src/main/java/com/negocio/stock/controller/RoleController.java
+++ src/main/java/com/negocio/stock/controller/RoleController.java
@@ -1,5 +1,30 @@
 package com.negocio.stock.controller;
 
+import com.negocio.stock.model.Role;
+import com.negocio.stock.repository.IRoleRepository;
+import com.negocio.stock.service.IRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/role")
 public class RoleController {
+
+    @Autowired
+    private IRoleService roleService;
+
+    @PostMapping
+    public ResponseEntity<Role> create(@RequestBody Role role){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(roleService.create(role));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Role> getById(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(roleService.findById(id));
+    }
+
 }
