eaed7e996e2bcb7baa09a0eef92996b182c6f5c9
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-03 15:44:58 -0300

    Se implementó el endpoint login y es totalmente funcional.
    AuthController.java

--- src/main/java/com/negocio/stock/controller/AuthController.java
+++ src/main/java/com/negocio/stock/controller/AuthController.java
@@ -1,8 +1,11 @@
 package com.negocio.stock.controller;
 
+import com.negocio.stock.dto.AuthLoginRequestDTO;
+import com.negocio.stock.dto.AuthLoginResponseDTO;
 import com.negocio.stock.dto.AuthRegisterRequestDTO;
 import com.negocio.stock.dto.AuthRegisterResponseDTO;
 import com.negocio.stock.service.IUserSecService;
+import com.negocio.stock.service.UserDetailsServiceImp;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,6 +21,8 @@ public class AuthController {
 
     @Autowired
     private IUserSecService userService;
+    @Autowired
+    private UserDetailsServiceImp userDetailsService;
 
     @PostMapping("/register")
     public ResponseEntity<AuthRegisterResponseDTO> register(@Valid @RequestBody AuthRegisterRequestDTO request){
@@ -25,4 +30,9 @@ public class AuthController {
                 .body(userService.register(request));
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<AuthLoginResponseDTO> login(@Valid @RequestBody AuthLoginRequestDTO request){
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(userDetailsService.loginUser(request));
+    }
 }
