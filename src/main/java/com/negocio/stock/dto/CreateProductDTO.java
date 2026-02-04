50f254ef6404c44bf015b5a03522fc2d02e5d850
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-04 01:23:40 -0300

    Implementación de dto para manejo de peticiones para alta de productos.
    CreateProductDTO.java

--- src/main/java/com/negocio/stock/dto/CreateProductDTO.java
+++ src/main/java/com/negocio/stock/dto/CreateProductDTO.java
@@ -1,8 +1,11 @@
 package com.negocio.stock.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.math.BigDecimal;
 
 public record CreateProductDTO(
+        @NotBlank
         String name,
         BigDecimal price,
         int stock,
