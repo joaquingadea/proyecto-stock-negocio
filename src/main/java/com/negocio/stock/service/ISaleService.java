b1d4cf9c7091e50f15fde3b186a6aa9a7faca75e
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-04 23:03:23 -0300

    Se agregó el metodo create.
    ISaleService.java

--- src/main/java/com/negocio/stock/service/ISaleService.java
+++ src/main/java/com/negocio/stock/service/ISaleService.java
@@ -1,4 +1,9 @@
 package com.negocio.stock.service;
 
+import com.negocio.stock.dto.MessageResponseDTO;
+import com.negocio.stock.dto.SaveSaleDTO;
+import org.jspecify.annotations.Nullable;
+
 public interface ISaleService {
+    MessageResponseDTO create(SaveSaleDTO request);
 }
