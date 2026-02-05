0f7b4702154ea36e231c51c0168c63bea3dca7cc
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-04 23:04:14 -0300

    Se implementaron los metodoss edit y deleteById.
    ProductService.java

--- src/main/java/com/negocio/stock/service/ProductService.java
+++ src/main/java/com/negocio/stock/service/ProductService.java
@@ -1,8 +1,11 @@
 package com.negocio.stock.service;
 
 import com.negocio.stock.dto.CreateProductDTO;
+import com.negocio.stock.dto.EditProductRequestDTO;
+import com.negocio.stock.dto.MessageResponseDTO;
 import com.negocio.stock.model.Product;
 import com.negocio.stock.repository.IProductRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +28,26 @@ public class ProductService implements IProductService{
     public Product read(Long id) {
         return productRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("No existe"));
+    }
+
+    @Override
+    public MessageResponseDTO edit(Long id, EditProductRequestDTO request) {
+        Product productRepo = productRepository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+        productRepo.setName(request.name());
+        productRepo.setPrice(request.price());
+        productRepo.setStock(request.stock());
+        productRepo.setDescription(request.description());
+
+        productRepository.save(productRepo);
+
+        return new MessageResponseDTO("Successfully edited product.");
+    }
+
+    @Override
+    public MessageResponseDTO deleteById(Long id) {
+        productRepository.deleteById(id);
+        return new MessageResponseDTO("Successfully deleted product.");
     }
 
 }
