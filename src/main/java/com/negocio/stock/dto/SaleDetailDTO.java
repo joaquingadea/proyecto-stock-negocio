8ee1e97fc9903efbea2e316302b12e1329edc86f
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-04 23:01:55 -0300

    Se agregó la etiqueta NonNull para el atributo quantity.
    SaleDetailDTO.java

--- src/main/java/com/negocio/stock/dto/SaleDetailDTO.java
+++ src/main/java/com/negocio/stock/dto/SaleDetailDTO.java
@@ -2,10 +2,12 @@ package com.negocio.stock.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record SaleDetailDTO(
         @NotBlank
         Long productId,
+        @NotNull
         @Min(1)
         int quantity
 ) {
