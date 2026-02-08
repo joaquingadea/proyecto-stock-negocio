923c88595ec73fa06703b467be5ff78d9e0b3995
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-08 20:07:02 -0300

    Se modificó el filtro para la validación de JWT's.
    JwtTokenValidator.java

--- src/main/java/com/negocio/stock/security/config/filter/JwtTokenValidator.java
+++ src/main/java/com/negocio/stock/security/config/filter/JwtTokenValidator.java
@@ -1,4 +1,68 @@
-package com.negocio.stock.security.filter;
+package com.negocio.stock.security.config.filter;
 
-public class JwtTokenValidator {
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.negocio.stock.utils.JwtUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.NonNull;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collection;
+
+public class JwtTokenValidator extends OncePerRequestFilter {
+
+    private JwtUtils jwtUtils;
+
+    public JwtTokenValidator(JwtUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+    }
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
+            throws ServletException, IOException
+    {
+        String headerAuthorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if(headerAuthorization == null){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        if (!headerAuthorization.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        String jwt = headerAuthorization.substring(7);
+        DecodedJWT decodedJwt = jwtUtils.decodeToken(jwt);
+
+        String username = jwtUtils.getUsername(decodedJwt);
+        String authorities = jwtUtils.getSpecificClaim(decodedJwt,"authorities").asString();
+
+        if (authorities == null){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        Collection<? extends GrantedAuthority> authorityList = AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
+
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = new UsernamePasswordAuthenticationToken(username,null,authorityList);
+        securityContext.setAuthentication(authentication);
+        SecurityContextHolder.setContext(securityContext);
+
+        filterChain.doFilter(request,response);
+    }
 }
