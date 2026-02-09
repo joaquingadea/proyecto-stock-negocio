91d29645f649ddf2989377f67b42a933447d8fd1
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-08 21:35:49 -0300

    Se eliminaron dependencias innecesarias.
    ISaleService.java

--- src/main/java/com/negocio/stock/service/ISaleService.java
+++ src/main/java/com/negocio/stock/service/ISaleService.java
@@ -2,7 +2,6 @@ package com.negocio.stock.service;
 
 import com.negocio.stock.dto.MessageResponseDTO;
 import com.negocio.stock.dto.SaveSaleDTO;
-import org.jspecify.annotations.Nullable;
 
 public interface ISaleService {
     MessageResponseDTO create(SaveSaleDTO request);
