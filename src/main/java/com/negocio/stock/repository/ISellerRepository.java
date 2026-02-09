626908df448cb046825fe8aadbb488772c55c10c
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-08 21:34:46 -0300

    Se agregó una query personalizada para obtener el ID del Seller a traves del username del UserSec relacionado.
    ISellerRepository.java

--- src/main/java/com/negocio/stock/repository/ISellerRepository.java
+++ src/main/java/com/negocio/stock/repository/ISellerRepository.java
@@ -2,8 +2,19 @@ package com.negocio.stock.repository;
 
 import com.negocio.stock.model.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ISellerRepository extends JpaRepository<Seller,Long> {
+    @Query("""
+        SELECT s.id
+        FROM Seller s
+        JOIN s.user u
+        WHERE u.username = :username
+    """)
+    Optional<Long> findSellerIdByUsername(@Param("username") String username);
 }
