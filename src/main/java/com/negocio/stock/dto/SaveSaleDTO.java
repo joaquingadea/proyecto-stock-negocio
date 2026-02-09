f50f8de794c97fc2571efec797c7176230b5138d
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-08 21:38:25 -0300

    Se agregó el ID del vendedor como atributo necesario para generar una venta.
    SaveSaleDTO.java

--- src/main/java/com/negocio/stock/dto/SaveSaleDTO.java
+++ src/main/java/com/negocio/stock/dto/SaveSaleDTO.java
@@ -1,11 +1,14 @@
 package com.negocio.stock.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
 public record SaveSaleDTO(
+        @NotNull
+        Long sellerId,
         @Size(min = 1)
         @NotEmpty
         List<SaleDetailDTO> details
