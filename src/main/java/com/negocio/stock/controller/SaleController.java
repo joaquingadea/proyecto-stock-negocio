85c8e3d7a624f16b9408ad88e54474e7e613cc7f
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-08 21:40:53 -0300

    Se agregó como parametro la autenticación y en la respuesta se agregó el DTO correspondiente.
    SaleController.java

--- src/main/java/com/negocio/stock/controller/SaleController.java
+++ src/main/java/com/negocio/stock/controller/SaleController.java
@@ -1,11 +1,13 @@
 package com.negocio.stock.controller;
 
+import com.negocio.stock.dto.MessageResponseDTO;
 import com.negocio.stock.dto.SaveSaleDTO;
 import com.negocio.stock.service.ISaleService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +21,7 @@ public class SaleController {
     private ISaleService saleService;
 
     @PostMapping
-    public ResponseEntity makeSale(@Valid @RequestBody SaveSaleDTO request){
+    public ResponseEntity<MessageResponseDTO> makeSale(@Valid @RequestBody SaveSaleDTO request, Authentication authentication){
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(saleService.create(request));
     }
