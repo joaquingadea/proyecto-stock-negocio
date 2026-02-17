a5cd314ca47c6c89effa6b292076c663a38bff5d
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-17 12:59:46 -0300

    Se creó e implementó el metodo getAllSales
    ISaleService.java y SaleService.java

--- src/main/java/com/negocio/stock/service/ISaleService.java
+++ src/main/java/com/negocio/stock/service/ISaleService.java
@@ -1,8 +1,12 @@
 package com.negocio.stock.service;
 
 import com.negocio.stock.dto.MessageResponseDTO;
-import com.negocio.stock.dto.SaveSaleDTO;
+import com.negocio.stock.dto.CreateSaleRequestDTO;
+import com.negocio.stock.model.Sale;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ISaleService {
-    MessageResponseDTO create(SaveSaleDTO request);
+    MessageResponseDTO create(CreateSaleRequestDTO request);
+    Page<Sale> getAllSales(Pageable pageRequest);
 }
--- src/main/java/com/negocio/stock/service/SaleService.java
+++ src/main/java/com/negocio/stock/service/SaleService.java
@@ -1,8 +1,8 @@
 package com.negocio.stock.service;
 
 import com.negocio.stock.dto.MessageResponseDTO;
-import com.negocio.stock.dto.SaleDetailDTO;
-import com.negocio.stock.dto.SaveSaleDTO;
+import com.negocio.stock.dto.SaleDetailRequestDTO;
+import com.negocio.stock.dto.CreateSaleRequestDTO;
 import com.negocio.stock.model.Product;
 import com.negocio.stock.model.Sale;
 import com.negocio.stock.model.SaleDetail;
@@ -14,14 +14,12 @@ import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @Validated
@@ -35,12 +33,12 @@ public class SaleService implements ISaleService{
     @Autowired
     private ISellerRepository sellerRepository;
 
-    /* - El proceso crea una nueva venta, inicializa el total de la venta en 0, busca el usuario a traves de su ID en la base de datos
+    /* - El proceso crea una nueva venta, inicializa el total de la venta en 0, busca el vendedor a traves de su ID en la base de datos
     *  - Valiida uno por uno los SaleDetails y en caso de que algo falle se hace un rollback a traves de la annotation @Transactional
     *  - Una vez validado todo lo necesario se terminan de settear los atributos necesarios para la nueva venta y se da el alta de la misma en la BD
     */
     @Override
-    public MessageResponseDTO create(@Valid SaveSaleDTO request) {
+    public MessageResponseDTO create(@Valid CreateSaleRequestDTO request) {
 
         Seller seller = sellerRepository.findById(request.sellerId())
                             .orElseThrow(() -> new EntityNotFoundException("No se encontro el vendedor con id: "+ request.sellerId()));
@@ -49,7 +47,7 @@ public class SaleService implements ISaleService{
 
         Sale newSale = new Sale();
 
-        for (SaleDetailDTO dto : request.details()){
+        for (SaleDetailRequestDTO dto : request.details()){
 
             Product product = productRepository.findById(dto.productId())
                     .orElseThrow(() -> new EntityNotFoundException("No se encontro el producto con id: "+ dto.productId()));
@@ -75,6 +73,11 @@ public class SaleService implements ISaleService{
         saleRepository.save(newSale);
 
         return new MessageResponseDTO("Successfully saved sale.");
+    }
+
+    @Override
+    public Page<Sale> getAllSales(Pageable pageRequest) {
+        return saleRepository.findAll(pageRequest);
     }
 
 }
