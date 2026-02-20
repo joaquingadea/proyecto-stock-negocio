daefe3b5b0ac1b32f518edfe235aa3baf1692059
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-19 21:27:06 -0300

    Se agregó los endpoints para ver los productos de manera paginada y el para eliminar una cantidad de productos en especifico.
    ProductController.java

--- src/main/java/com/negocio/stock/controller/ProductController.java
+++ src/main/java/com/negocio/stock/controller/ProductController.java
@@ -6,9 +6,16 @@ import com.negocio.stock.dto.MessageResponseDTO;
 import com.negocio.stock.model.Product;
 import com.negocio.stock.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
@@ -18,7 +25,7 @@ public class ProductController {
     private IProductService productService;
 
     @PostMapping
-    public ResponseEntity create(@RequestBody CreateProductRequestDTO request){
+    public ResponseEntity<Product> create(@RequestBody CreateProductRequestDTO request){
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(productService.create(request));
     }
@@ -27,6 +34,11 @@ public class ProductController {
     public ResponseEntity<Product> read(@PathVariable Long id){
         return ResponseEntity.status(HttpStatus.OK)
                 .body(productService.read(id));
+    }
+    @GetMapping
+    public ResponseEntity<Page<Product>> getAllProducts(Pageable pageable, Authentication authentication){
+        Pageable pageableRequest = PageRequest.of(pageable.getPageNumber(), 15, Sort.by(Sort.Direction.ASC,"name"));
+        return ResponseEntity.status(HttpStatus.OK).body(productService.getAllProducts(pageableRequest));
     }
 
     @PatchMapping("/{id}")
@@ -39,5 +51,10 @@ public class ProductController {
     public ResponseEntity<MessageResponseDTO> deleteById(@PathVariable Long id){
         return ResponseEntity.status(HttpStatus.OK)
                 .body(productService.deleteById(id));
+    }
+    @DeleteMapping
+    public ResponseEntity<MessageResponseDTO> deleteSomeById(@RequestBody List<Long> ids){
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(productService.deleteSomeById(ids));
     }
 }
