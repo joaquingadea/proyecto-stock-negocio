6eccaf3efed4afc5bb0fa16322ff5bcdf3da6973
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-28 01:05:51 -0300

    Se agrego el metodo getAllSales.
    ISaleService.java

--- src/main/java/com/negocio/stock/service/ISaleService.java
+++ src/main/java/com/negocio/stock/service/ISaleService.java
@@ -1,12 +1,14 @@
 package com.negocio.stock.service;
 
+import com.negocio.stock.dto.GetSaleResponseDTO;
 import com.negocio.stock.dto.MessageResponseDTO;
 import com.negocio.stock.dto.CreateSaleRequestDTO;
 import com.negocio.stock.model.Sale;
+import org.jspecify.annotations.Nullable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface ISaleService {
     MessageResponseDTO create(CreateSaleRequestDTO request);
-    Page<Sale> getAllSales(Pageable pageRequest);
+    Page<GetSaleResponseDTO> getAllSales(Pageable pageRequest);
 }
