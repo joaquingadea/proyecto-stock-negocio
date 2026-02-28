be37828045e89e042d9851b00382b4f4eeab056c
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-28 01:10:42 -0300

    Implementacion del metodo findAllBySaleId
    SaleDetailService.java

--- src/main/java/com/negocio/stock/service/SaleDetailService.java
+++ src/main/java/com/negocio/stock/service/SaleDetailService.java
@@ -1,7 +1,20 @@
 package com.negocio.stock.service;
 
+import com.negocio.stock.model.SaleDetail;
+import com.negocio.stock.repository.ISaleDetailRepository;
+import org.jspecify.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SaleDetailService implements ISaleDetailService{
+    @Autowired
+    private ISaleDetailRepository saleDetailRepository;
+
+    @Override
+    public @Nullable List<SaleDetail> findAllBySaleId(Long id) {
+        return saleDetailRepository.findAllBySaleId(id);
+    }
 }
