0a17ccfa920f45bd579b14838402eda97d30b543
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-28 01:06:27 -0300

    Se agrego el metodo getSellers.
    ISellerService.java

--- src/main/java/com/negocio/stock/service/ISellerService.java
+++ src/main/java/com/negocio/stock/service/ISellerService.java
@@ -1,4 +1,10 @@
 package com.negocio.stock.service;
 
+import com.negocio.stock.dto.SellerResponseDTO;
+import org.jspecify.annotations.Nullable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
 public interface ISellerService {
+    @Nullable Page<SellerResponseDTO> getSellers(PageRequest pageRequest);
 }
