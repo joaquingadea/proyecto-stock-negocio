1b3713ba50a48f6ac853cb054c0d485404ca5b4d
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-28 01:07:39 -0300

    Se agregaron los metodos getGuestUsers, setRole, removeRole, removeRoles y deleteById.
    IUserSecService.java

--- src/main/java/com/negocio/stock/service/IUserSecService.java
+++ src/main/java/com/negocio/stock/service/IUserSecService.java
@@ -2,6 +2,23 @@ package com.negocio.stock.service;
 
 import com.negocio.stock.dto.AuthRegisterRequestDTO;
 import com.negocio.stock.dto.AuthRegisterResponseDTO;
+import com.negocio.stock.dto.GuestUserResponseDTO;
+import org.jspecify.annotations.Nullable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
+
 public interface IUserSecService {
     AuthRegisterResponseDTO register(AuthRegisterRequestDTO request);
+
+    @Nullable Page<GuestUserResponseDTO> getGuestUsers(PageRequest pageRequest);
+
+    void setRole(Long id,String name);
+
+    void removeRole(Long id, String name);
+
+    void removeRoles(Long id, List<String> roles);
+
+    void deleteById(Long id);
 }
