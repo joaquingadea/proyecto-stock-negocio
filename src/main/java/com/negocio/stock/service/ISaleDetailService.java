53b37de3ecaba5bd0e4f74960c765dae509917f3
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-28 01:05:20 -0300

    Se agrego el metodo findAllBySaleId.
    ISaleDetailService.java

--- src/main/java/com/negocio/stock/service/ISaleDetailService.java
+++ src/main/java/com/negocio/stock/service/ISaleDetailService.java
@@ -1,4 +1,10 @@
 package com.negocio.stock.service;
 
+import com.negocio.stock.model.SaleDetail;
+import org.jspecify.annotations.Nullable;
+
+import java.util.List;
+
 public interface ISaleDetailService {
+    List<SaleDetail> findAllBySaleId(Long id);
 }
