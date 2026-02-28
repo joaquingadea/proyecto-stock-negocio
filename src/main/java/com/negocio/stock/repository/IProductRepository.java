2b233be29a7ab9bcb8f84cdfd34d5163cea43787
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-28 00:53:09 -0300

    Se agrego el metodo findProductsWithStock(devolviendo los ids y los nombres de la tabla con stock) con la query explicita hecha con JPQL.
    IProductRepository.java

--- src/main/java/com/negocio/stock/repository/IProductRepository.java
+++ src/main/java/com/negocio/stock/repository/IProductRepository.java
@@ -1,9 +1,19 @@
 package com.negocio.stock.repository;
 
+import com.negocio.stock.dto.ProductNameIdDTO;
 import com.negocio.stock.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IProductRepository extends JpaRepository<Product,Long> {
+    List<ProductNameIdDTO> findAllBy();
+    @Query("""
+            SELECT p.id AS id, p.name AS name FROM Product p
+            WHERE p.stock > 0
+            """)
+    List<ProductNameIdDTO> findProductsWithStock();
 }
