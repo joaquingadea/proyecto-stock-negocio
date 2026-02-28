38bb41e94cd5814769275d4caaf3b85d6289d44e
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-28 00:54:48 -0300

    Se agrego el metodo findAllBySaleId.
    ISaleDetailRepository.java

--- src/main/java/com/negocio/stock/repository/ISaleDetailRepository.java
+++ src/main/java/com/negocio/stock/repository/ISaleDetailRepository.java
@@ -1,9 +1,13 @@
 package com.negocio.stock.repository;
 
 import com.negocio.stock.model.SaleDetail;
+import org.jspecify.annotations.Nullable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ISaleDetailRepository extends JpaRepository<SaleDetail,Long> {
+    List<SaleDetail> findAllBySaleId(Long id);
 }
