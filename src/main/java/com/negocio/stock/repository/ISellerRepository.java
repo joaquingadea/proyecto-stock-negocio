f6595eea72d005e8ac2fdf25ee62a9c864329e11
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-28 00:56:36 -0300

    Se agrego el metodo findSellersByRole con la query explicita en JPQL.
    ISellerRepository.java

--- src/main/java/com/negocio/stock/repository/ISellerRepository.java
+++ src/main/java/com/negocio/stock/repository/ISellerRepository.java
@@ -1,6 +1,9 @@
 package com.negocio.stock.repository;
 
+import com.negocio.stock.model.Role;
 import com.negocio.stock.model.Seller;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,4 +20,14 @@ public interface ISellerRepository extends JpaRepository<Seller,Long> {
         WHERE u.username = :username
     """)
     Optional<Long> findSellerIdByUsername(@Param("username") String username);
+
+    @Query("""
+    SELECT DISTINCT s
+    FROM Seller s
+    JOIN s.user u
+    JOIN u.roleList r
+    WHERE r = :role
+    ORDER BY s.id DESC
+""")
+    Page<Seller> findSellersByRole(@Param("role") Role role, Pageable pageable);
 }
