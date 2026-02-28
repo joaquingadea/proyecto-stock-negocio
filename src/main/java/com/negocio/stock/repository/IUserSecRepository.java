94257236abbd3e665401e8ab38ae67941f278ec0
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-28 00:57:36 -0300

    Se agrego el metodo findUsersByRole con la query explicita en JPQL.
    IUserSecRepository.java

--- src/main/java/com/negocio/stock/repository/IUserSecRepository.java
+++ src/main/java/com/negocio/stock/repository/IUserSecRepository.java
@@ -1,13 +1,25 @@
 package com.negocio.stock.repository;
 
 import com.negocio.stock.model.UserSec;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface IUserSecRepository extends JpaRepository<UserSec,Long> {
+
     Optional<UserSec> findUserByUsername(String username);
-    Optional<Long> findIdByUsername(String username);
+
+    @Query("""
+    SELECT DISTINCT u
+    FROM UserSec u
+    JOIN u.roleList r
+    WHERE r.name = :roleName
+    """)
+    Page<UserSec> findUsersByRole(@Param("roleName") String roleName, Pageable pageable);
 }
