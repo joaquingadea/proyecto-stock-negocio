b8ab00626a01c436fcae38e86d3d36a29126debc
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-02-28 01:12:43 -0300

    Implementacion del metodo getSellers
    SellerService.java

--- src/main/java/com/negocio/stock/service/SellerService.java
+++ src/main/java/com/negocio/stock/service/SellerService.java
@@ -1,7 +1,31 @@
 package com.negocio.stock.service;
 
+import com.negocio.stock.dto.SellerResponseDTO;
+import com.negocio.stock.repository.IRoleRepository;
+import com.negocio.stock.repository.ISaleRepository;
+import com.negocio.stock.repository.ISellerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SellerService implements ISellerService{
+
+    @Autowired
+    private ISellerRepository sellerRepository;
+    @Autowired
+    private ISaleRepository saleRepository;
+    @Autowired
+    private IRoleRepository roleRepository;
+
+    @Override
+    public Page<SellerResponseDTO> getSellers(PageRequest pageRequest) {
+        return sellerRepository.findSellersByRole(roleRepository.findByName("USER").orElseThrow(),pageRequest)
+                .map(seller -> new SellerResponseDTO(
+                            seller.getId(),
+                            seller.getUser().getUsername(),
+                            saleRepository.countBySellerId(seller.getId())
+        ));
+    }
 }
