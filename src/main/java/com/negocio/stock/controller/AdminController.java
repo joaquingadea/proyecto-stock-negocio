52b1ec684450a26fcc414ee697fae5ffb04cc14c
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-03-06 00:29:31 -0300

    Se estableció la seguridad a nivel metodo de los endpoints
    AdminController.java

--- src/main/java/com/negocio/stock/controller/AdminController.java
+++ src/main/java/com/negocio/stock/controller/AdminController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
 
     @Autowired
