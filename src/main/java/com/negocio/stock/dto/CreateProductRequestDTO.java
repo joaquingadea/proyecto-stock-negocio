0bf4ba0b9c1fe88ae515b6435676de6c5b776ed8
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-03-20 20:23:33 -0300

    Se agregaron las validaciones correspondientes para crear los productos.
    CreateProductRequestDTO.java

--- src/main/java/com/negocio/stock/dto/CreateProductRequestDTO.java
+++ src/main/java/com/negocio/stock/dto/CreateProductRequestDTO.java
@@ -1,5 +1,6 @@
 package com.negocio.stock.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 import java.math.BigDecimal;
@@ -7,7 +8,9 @@ import java.math.BigDecimal;
 public record CreateProductRequestDTO(
         @NotBlank
         String name,
+        @Min(value = 0)
         BigDecimal price,
+        @Min(value = 0)
         int stock,
         String description
 ) {
