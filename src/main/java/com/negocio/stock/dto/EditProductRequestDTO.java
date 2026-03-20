1d3844e1fa46d97fd9317fc9cf5ba2296210cd65
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-03-20 20:22:47 -0300

    Se agregaron las validaciones correspondientes para editar los productos.
    EditProductRequestDTO.java

--- src/main/java/com/negocio/stock/dto/EditProductRequestDTO.java
+++ src/main/java/com/negocio/stock/dto/EditProductRequestDTO.java
@@ -1,14 +1,19 @@
 package com.negocio.stock.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 
 public record EditProductRequestDTO(
         @NotBlank
         String name,
+        @Min(value = 0)
         BigDecimal price,
+        @Min(value = 0)
         int stock,
+
         String description
 ) {
 }
