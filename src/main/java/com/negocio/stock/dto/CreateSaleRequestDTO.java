658f9929e27706da81025d06a8126c1c2c6b6f7c
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-03-20 20:24:02 -0300

    Se agregaron las validaciones correspondientes para crear las ventas.
    CreateSaleRequestDTO.java

--- src/main/java/com/negocio/stock/dto/CreateSaleRequestDTO.java
+++ src/main/java/com/negocio/stock/dto/CreateSaleRequestDTO.java
@@ -7,8 +7,6 @@ import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record CreateSaleRequestDTO(
-        @NotNull
-        Long sellerId,
         @Size(min = 1)
         @NotEmpty
         List<SaleDetailRequestDTO> details
