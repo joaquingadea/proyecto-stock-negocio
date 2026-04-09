33765cdcb846580cec7b8cf9d63b68777cdcf306
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-09 16:46:38 -0300

    Se agregó el endpoint my-sales para obtener tus ventas en caso de ser vendedor o administrador.
    SaleController.java

--- src/main/java/com/negocio/stock/controller/SaleController.java
+++ src/main/java/com/negocio/stock/controller/SaleController.java
@@ -5,6 +5,7 @@ import com.negocio.stock.dto.MessageResponseDTO;
 import com.negocio.stock.dto.CreateSaleRequestDTO;
 import com.negocio.stock.service.ISaleDetailService;
 import com.negocio.stock.service.ISaleService;
+import com.negocio.stock.service.ISellerService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -26,12 +27,14 @@ public class SaleController {
     private ISaleService saleService;
     @Autowired
     private ISaleDetailService saleDetailService;
+    @Autowired
+    private ISellerService sellerService;
 
     @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
     @PostMapping
     public ResponseEntity<MessageResponseDTO> makeSale(@Valid @RequestBody CreateSaleRequestDTO request, Authentication authentication){
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(saleService.create(request));
+                .body(saleService.create(request,authentication.getName()));
     }
     @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
@@ -42,5 +45,17 @@ public class SaleController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(saleService.getAllSales(pageRequest));
+    }
+    @PreAuthorize("hasAuthority('ROLE_USER') or hasAuthority('ROLE_ADMIN')")
+    @GetMapping("/my-sales")
+    public ResponseEntity<Page<GetSaleResponseDTO>> getMySales(@PageableDefault(page = 0,size = 15) Pageable pageable, Authentication authentication){
+
+        String username = authentication.getName();
+
+        Pageable pageRequest = PageRequest.of(pageable.getPageNumber(),15, Sort.by(Sort.Direction.DESC,"date"));
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(saleService.getMySales(username,pageRequest));
     }
 }
