1e24de784e2e3d18da756cbd5dd67646a1baec31
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-09 21:22:18 -0300

    Se implementaron los metodos findGuestUsers, findRolesByUserId y findSellerIdByUsername con JPQL.
    IUserSecRepository.java

--- src/main/java/com/negocio/stock/repository/IUserSecRepository.java
+++ src/main/java/com/negocio/stock/repository/IUserSecRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -22,4 +23,30 @@ public interface IUserSecRepository extends JpaRepository<UserSec,Long> {
     WHERE r.name = :roleName
     """)
     Page<UserSec> findUsersByRole(@Param("roleName") String roleName, Pageable pageable);
+    @Query("""
+    SELECT DISTINCT u
+    FROM UserSec u
+    JOIN u.roleList r
+    JOIN u.seller s
+    WHERE r.name = 'GUEST'
+      AND SIZE(u.roleList) = 1
+      AND s.sellerRequested = true
+      AND s.sellerApproved = false
+""")
+    Page<UserSec> findGuestUsers(Pageable pageable);
+    @Query(value = """
+    SELECT r.name
+    FROM user_role ur
+    JOIN roles r ON ur.role_id = r.id
+    WHERE ur.user_id = :userId
+    """, nativeQuery = true)
+    List<String> findRolesByUserId(Long userId);
+
+    @Query("""
+    SELECT s.id
+    FROM Seller s
+    JOIN s.user u
+    WHERE u.username = :name
+    """)
+    Optional<Long> findSellerIdByUsername(String name);
 }
