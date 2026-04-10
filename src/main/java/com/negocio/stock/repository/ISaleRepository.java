8828724ceedae121c0105035598658c669ed6d76
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-09 21:19:21 -0300

    Se implementó el método findAllBySellerUserUsername con JPQL.
    ISaleRepository.java

--- src/main/java/com/negocio/stock/repository/ISaleRepository.java
+++ src/main/java/com/negocio/stock/repository/ISaleRepository.java
@@ -1,10 +1,20 @@
 package com.negocio.stock.repository;
 
+import com.negocio.stock.dto.GetSaleResponseDTO;
 import com.negocio.stock.model.Sale;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ISaleRepository extends JpaRepository<Sale,Long> {
     long countBySellerId(Long id);
+
+    @Query("""
+            SELECT s FROM Sale s WHERE s.seller.user.username = :username
+            """)
+    Page<Sale> findAllBySellerUserUsername(@Param("username") String username, Pageable pageRequest);
 }
