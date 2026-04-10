d9753a04b0e8b103991f1627770cb3c715e4c951
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-09 21:20:53 -0300

    Implementacion de los metodos findUserIdBySellerId, isSellerRequestedById y findByUserUsername con JPQL.
    ISellerRepository.java

--- src/main/java/com/negocio/stock/repository/ISellerRepository.java
+++ src/main/java/com/negocio/stock/repository/ISellerRepository.java
@@ -2,6 +2,7 @@ package com.negocio.stock.repository;
 
 import com.negocio.stock.model.Role;
 import com.negocio.stock.model.Seller;
+import com.negocio.stock.model.UserSec;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -20,14 +21,29 @@ public interface ISellerRepository extends JpaRepository<Seller,Long> {
         WHERE u.username = :username
     """)
     Optional<Long> findSellerIdByUsername(@Param("username") String username);
-
     @Query("""
     SELECT DISTINCT s
     FROM Seller s
     JOIN s.user u
     JOIN u.roleList r
-    WHERE r = :role
+    WHERE r.name IN ('USER', 'ADMIN')
+      AND NOT EXISTS (
+          SELECT 1
+          FROM u.roleList r2
+          WHERE r2.name = 'GUEST'
+      )
     ORDER BY s.id DESC
 """)
-    Page<Seller> findSellersByRole(@Param("role") Role role, Pageable pageable);
+    Page<Seller> findSellersWithUserOrAdmin(Pageable pageable);
+
+    @Query("SELECT s.user.id FROM Seller s WHERE s.id = :sellerId")
+    Optional<Long> findUserIdBySellerId(@Param("sellerId") Long sellerId);
+
+
+    @Query("SELECT s.sellerRequested FROM Seller s WHERE s.id = :sellerId")
+    boolean isSellerRequestedById(Long sellerId);
+
+    @Query("SELECT s FROM Seller s WHERE s.user.username = :username")
+    Optional<Seller> findByUserUsername(String username);
 }
+
