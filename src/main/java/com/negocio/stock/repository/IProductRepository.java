47f6c2e242d88357673b22d9539563f753aae86a
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-09 21:17:59 -0300

    Se implementaron las querys findProductsWithStock, findAllActivated y findAllDesactivated con JPQL.
    IProductRepository.java

--- src/main/java/com/negocio/stock/repository/IProductRepository.java
+++ src/main/java/com/negocio/stock/repository/IProductRepository.java
@@ -1,7 +1,9 @@
 package com.negocio.stock.repository;
 
-import com.negocio.stock.dto.ProductNameIdDTO;
+import com.negocio.stock.dto.ProductNameIdStockPriceDTO;
 import com.negocio.stock.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,8 +13,21 @@ import java.util.List;
 @Repository
 public interface IProductRepository extends JpaRepository<Product,Long> {
     @Query("""
-            SELECT p.id AS id, p.name AS name FROM Product p
-            WHERE p.stock > 0
+            SELECT p.id AS id, p.name AS name, p.stock AS stock, p.price AS price FROM Product p
+            WHERE p.stock > 0 AND p.activated
             """)
-    List<ProductNameIdDTO> findProductsWithStock();
+    List<ProductNameIdStockPriceDTO> findProductsWithStock();
+
+    @Query("""
+       SELECT p
+       FROM Product p
+       WHERE p.activated
+       """)
+    Page<Product> findAllActivated(Pageable pageable);
+    @Query("""
+       SELECT p
+       FROM Product p
+       WHERE p.activated = false
+       """)
+    Page<Product> findAllDesactivated(Pageable pageableRequest);
 }
