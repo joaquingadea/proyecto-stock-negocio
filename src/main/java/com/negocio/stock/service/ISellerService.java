e3cb7f14436af17b71653173cab17948ada8eb69
Joaquin Adolfo Gadea <joaquingadea485@gmail.com>
2026-04-12 19:38:17 -0300

    Se crearon e implementaron los métodos applyForSeller, denySellerRequest, acceptSellerRequest y isSellerRequested.
    SellerService.java y ISellerService.java

--- src/main/java/com/negocio/stock/service/ISellerService.java
+++ src/main/java/com/negocio/stock/service/ISellerService.java
@@ -1,10 +1,17 @@
 package com.negocio.stock.service;
 
+import com.negocio.stock.dto.GetSaleResponseDTO;
 import com.negocio.stock.dto.SellerResponseDTO;
 import org.jspecify.annotations.Nullable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 public interface ISellerService {
     @Nullable Page<SellerResponseDTO> getSellers(PageRequest pageRequest);
+    void applyForSeller(Long sellerId);
+    void denySellerRequest(Long sellerId);
+    void acceptSellerRequest(Long sellerId);
+    boolean isSellerRequested(Long sellerId);
+
 }
--- src/main/java/com/negocio/stock/service/SellerService.java
+++ src/main/java/com/negocio/stock/service/SellerService.java
@@ -1,9 +1,11 @@
 package com.negocio.stock.service;
 
 import com.negocio.stock.dto.SellerResponseDTO;
+import com.negocio.stock.model.Seller;
 import com.negocio.stock.repository.IRoleRepository;
 import com.negocio.stock.repository.ISaleRepository;
 import com.negocio.stock.repository.ISellerRepository;
+import com.negocio.stock.repository.IUserSecRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -18,14 +20,50 @@ public class SellerService implements ISellerService{
     private ISaleRepository saleRepository;
     @Autowired
     private IRoleRepository roleRepository;
+    @Autowired
+    private IUserSecRepository userRepository;
+    @Autowired
+    private IUserSecService userService;
 
     @Override
     public Page<SellerResponseDTO> getSellers(PageRequest pageRequest) {
-        return sellerRepository.findSellersByRole(roleRepository.findByName("USER").orElseThrow(),pageRequest)
+
+        return sellerRepository.findSellersWithUserOrAdmin(pageRequest)
                 .map(seller -> new SellerResponseDTO(
                             seller.getId(),
                             seller.getUser().getUsername(),
-                            saleRepository.countBySellerId(seller.getId())
+                            saleRepository.countBySellerId(seller.getId()),
+                            userRepository.findRolesByUserId(seller.getUser().getId())
         ));
     }
+    @Override
+    public void applyForSeller(Long sellerId) {
+        Seller sellerRepo = sellerRepository.findById(sellerId).orElseThrow();
+        sellerRepo.setSellerRequested(true);
+        sellerRepository.save(sellerRepo);
+    }
+
+    @Override
+    public void denySellerRequest(Long sellerId) {
+        Seller sellerRepo = sellerRepository.findById(sellerId).orElseThrow();
+        sellerRepo.setSellerRequested(false);
+        sellerRepository.save(sellerRepo);
+    }
+
+    @Override
+    public void acceptSellerRequest(Long sellerId) {
+        Seller sellerRepo = sellerRepository.findById(sellerId).orElseThrow();
+        Long userId = sellerRepo.getUser().getId();
+        userService.setRole(userId,"USER");
+        userService.removeRole(userId,"GUEST");
+        sellerRepo.setSellerApproved(true);
+        sellerRepository.save(sellerRepo);
+    }
+
+    @Override
+    public boolean isSellerRequested(Long sellerId) {
+        return sellerRepository.isSellerRequestedById(sellerId);
+    }
+
+
 }
